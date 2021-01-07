360cca7663228714622cfc59c85ecd08b0b2de71
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2021-01-08 00:55:53 +0330

    feat: add MText

---
+++ src/components/basics/MText.js
@@ -0,0 +1,48 @@
+// @flow
+
+import * as React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { colors, fonts } from '../../constants';
+
+type Props = {
+  fontStyle?: 'bold' | 'medium' | 'light',
+  variant?: 'error' | 'success' | 'normal',
+  textStyle?: Object,
+  children?: any,
+};
+
+const MText = function ({
+  fontStyle = 'medium',
+  variant = 'normal',
+  textStyle = {},
+  children,
+}: Props): React$Node {
+  return (
+    <Text style={[style[fontStyle], style[variant], textStyle]}>
+      {children}
+    </Text>
+  );
+};
+
+const style = StyleSheet.create({
+  medium: {
+    fontFamily: fonts.medium,
+  },
+  bold: {
+    fontFamily: fonts.bold,
+  },
+  light: {
+    fontFamily: fonts.light,
+  },
+  error: {
+    color: colors.red,
+  },
+  success: {
+    color: colors.green,
+  },
+  normal: {
+    color: colors.white,
+  },
+});
+
+export default MText;
