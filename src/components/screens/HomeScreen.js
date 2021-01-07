5c9332ddb062323c840bad3f6ba4f057387cad45
Melika Norouzbeygi <melikanorouzbeygi75@gmail.com>
2021-01-08 01:04:13 +0330

    feat: finish login

---
+++ src/components/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { colors } from '../../constants';
+
+const HomeScreen = function () {
+  return (
+    <View>
+      <Text>{colors.green}</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
